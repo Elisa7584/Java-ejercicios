4702384832410cddda2a712ea2b7fd1d9c055ea2
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-01-19 12:07:47 +0100

    Update buclesMarquis.java

--- buclesMarquis.java
+++ buclesMarquis.java
@@ -1,1 +1,8 @@
+// Criar um objeto da classe Random
+        Random rand = new Random();
 
+        // Gerar um número aleatório entre 1 e 10
+        int numeroAleatorio = rand.nextInt(10) + 1;
+
+        // Exibir o número aleatório gerado
+        System.out.println("Número aleatório entre 1 e 10: " + numeroAleatorio);
